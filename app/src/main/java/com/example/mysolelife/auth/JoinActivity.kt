d0a91ec4563f8c532a8b576677199db51eaa4470
82103 <kim5472678@gmail.com>
2023-08-27 16:31:53 +0900

    레이아웃 파일에서 <layout>태그로 안감싸줘서 ActivityJoinBinding이 안되고 있었음ㅜ

--- app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
@@ -2,12 +2,38 @@ package com.example.mysolelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding : ActivityJoinBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        auth = Firebase.auth
+
+    //코드상에 버튼id를 기입하지도 않았는데 어떻게 버튼을 누르면 회원가입이 되는건지?
+//        auth.createUserWithEmailAndPassword("abc@abc.com", "abcabc")
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//
+//                    Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
+//
+//                } else {
+//
+//                    Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
+//                }
+//            }
     }
 }
