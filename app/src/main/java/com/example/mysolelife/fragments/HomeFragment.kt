68449a6fb76ec4f521434a4e9577342a8b3aae6e
82103 <kim5472678@gmail.com>
2023-09-06 17:00:50 +0900

    해당 이미지를 누르면 그 페이지로 넘어갈 수 있게 코드 작성

--- app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
@@ -1,12 +1,14 @@
 package com.example.mysolelife.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.mysolelife.R
 import com.example.mysolelife.databinding.FragmentHomeBinding
 
@@ -26,7 +28,22 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         binding.tipTap.setOnClickListener {
-            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+            Log.d("HomeFragment", "tipTap")
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+        }
+
+        binding.talkTap.setOnClickListener {
+
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+
+        }
+
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookMarkFragment)
+        }
+
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
         }
         return binding.root
     }
