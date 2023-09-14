63ac8364e24838e1b6612065b17cc5f826422cd2
82103 <kim5472678@gmail.com>
2023-09-14 16:12:26 +0900

    어댑터에 리스트 a, b, c 전달

---
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentsListActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mysolelife.contentsList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mysolelife.R
+
+class ContentsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contents_list)
+
+        val rv : RecyclerView = findViewById(R.id.rv)
+
+        val items = ArrayList<String>()
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        val rvAdapter = ContentsRVAdapter(items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = GridLayoutManager(this, 2)
+    }
+}
