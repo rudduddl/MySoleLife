fb85aeba0f82c53c3b841a2be6ff08354c67a75a
82103 <kim5472678@gmail.com>
2023-09-15 23:01:42 +0900

    webUrl 추가

--- app/src/main/java/com/example/mysolelife/contentsList/ContentsModel.kt
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentsModel.kt
@@ -2,5 +2,6 @@ package com.example.mysolelife.contentsList
 
 data class ContentsModel (
     var title : String = "",
-    var imageUrl : String = ""
+    var imageUrl : String = "",
+    var webUrl : String = ""
 )
