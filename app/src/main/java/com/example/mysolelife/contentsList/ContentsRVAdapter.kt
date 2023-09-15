14a7105849c9185e815745094d24ccff4d3cf799
82103 <kim5472678@gmail.com>
2023-09-15 23:01:34 +0900

    아이템 클릭하면 해당 자료 넘겨주는 코드 삽입

--- app/src/main/java/com/example/mysolelife/contentsList/ContentsRVAdapter.kt
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentsRVAdapter.kt
@@ -1,18 +1,34 @@
 package com.example.mysolelife.contentsList
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mysolelife.R
 
-class ContentsRVAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<ContentsRVAdapter.ViewHolder>() {
+class ContentsRVAdapter(val context : Context, val items : ArrayList<ContentsModel>) : RecyclerView.Adapter<ContentsRVAdapter.ViewHolder>() {
+
+    interface ItemClick{
+        fun onClick(View : View, position: Int)
+    }
+    var itemClick : ItemClick? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentsRVAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.contents_rv_item, parent, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ContentsRVAdapter.ViewHolder, position: Int) {
+
+        if(itemClick != null){
+            holder.itemView.setOnClickListener{ v->
+                itemClick?.onClick(v, position)
+            }
+        }
         holder.bindItems(items[position])
     }
 
@@ -22,8 +38,16 @@ class ContentsRVAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<Co
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
-        fun bindItems(item : String) {
+        fun bindItems(item : ContentsModel) {
 
+            val contentsTitle = itemView.findViewById<TextView>(R.id.textArea)
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+
+
+            contentsTitle.text = item.title
+            Glide.with(context)
+                .load(item.imageUrl)
+                .into(imageViewArea)
         }
     }
 }
